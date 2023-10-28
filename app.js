7808ebe217d86e2a0f4245cdbe0a473590bfdc52
Dipokal HHJ <hhj@devent.kr>
2023-10-28 18:29:59 +0900

    fix: remove https

--- app.js
+++ app.js
@@ -10,19 +10,23 @@ app.disable('x-powered-by');
 app.use('/public', express.static('public'));
 app.use('/', express.static('dist'));
 
-const options = {
-    key: fs.readFileSync('./key/private.pem'),
-    cert: fs.readFileSync('./key/public.pem')
-};
 
-if (process.env.NODE_ENV == "production") {
-    app.listen(port, err => {
-        console.log(`[ + ] The server is running.`);
-    });
-} else {
-    const server = https.createServer(options, app);
 
-    server.listen(port, () => {
-      console.log("HTTPS server listening on port " + port);
-    });
-}
+// if (process.env.NODE_ENV == "production") {
+
+// } else {
+//     const options = {
+//         key: fs.readFileSync('./key/private.pem'),
+//         cert: fs.readFileSync('./key/public.pem')
+//     };
+
+//     const server = https.createServer(options, app);
+
+//     server.listen(port, () => {
+//       console.log("HTTPS server listening on port " + port);
+//     });
+// }    
+
+app.listen(port, err => {
+    console.log(`[ + ] The server is running.`);
+});
