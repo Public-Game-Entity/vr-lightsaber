7885c222f762c24c66a2a261006ad54b4cb9dd01
Dipokal HHJ <hhj@devent.kr>
2023-11-02 16:01:55 +0900

    feat: update button padding

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ type ButtonType = {
 
 const Button = ({ children, isActive = false, onClick }: ButtonType) => {
     return (
-        <button onClick={onClick} style={{ outline: "none", border: "1px solid rgb(255, 255, 255)", backgroundColor: isActive? "#ffffff" : "transparent", color: isActive? "#000000" : "#ffffff", padding: "0.8rem 0.5rem", borderRadius: "4px" }}>
+        <button onClick={onClick} style={{ outline: "none", border: "1px solid rgb(255, 255, 255)", backgroundColor: isActive? "#ffffff" : "transparent", color: isActive? "#000000" : "#ffffff", padding: "0.8rem 1rem", borderRadius: "4px" }}>
             {children}
         </button>
     );
